e2a4820934f2d49b850bbdd288238291ac2e6eda
Seth Perts <seth.robert.perts@gmail.com>
2022-04-29 18:17:21 +0000

    Add comments to runDocker.sh

--- runDocker.sh
+++ runDocker.sh
@@ -7,7 +7,7 @@ docker rm -f server
 #   NOTE!!!!!!!!
 #   The bind mount depends upon the database.db being in the correct location
 #   Ensure that database.db exists before running the docker container otherwise there will be no data saved between runs
-docker run --name server --network=host -p 3000:3000 --mount type=bind,src=database.db,dst=/opt/app/database.db product-organizer
+docker run --name server --network=host -p 3000:3000 --mount type=bind,src=/home/ec2-user/product-organizer/database.db,dst=/opt/app/database.db product-organizer
 
 # Print out the current containers and images on the system
 docker ps -a
